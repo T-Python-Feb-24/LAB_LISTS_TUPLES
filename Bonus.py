fb9b884b6814c7ed63ad4349e0945bcc8c83cee1
WissamZaidi <wzaidi@stu.kau.edu.sa>
2024-02-08 16:12:04 +0300

    ✅ Done Bonus

---
+++ Bonus.py
@@ -0,0 +1,22 @@
+# Movie Ratings Analysis
+
+movies = [
+    ("The Shawshank Redemption", 1994, [9, 10, 10, 9, 8, 9]),
+    ("The Godfather", 1972, [10, 9, 8, 10, 9, 7]),
+    ("Pulp Fiction", 1994, [9, 8, 7, 8, 6, 5]),
+    ("The Dark Knight", 2008, [10, 9, 9, 8, 9, 8]),
+    ("Schindler's List", 1993, [8, 9, 9, 7, 6, 8]),
+    ("The Room", 2003, [1, 2, 3, 4, 5, 1])
+]
+movies_Rated = []
+for movie in movies:
+    title = movie[0]
+    release_date = movie[1]
+
+    average_rating = round(sum(movie[2])/len(movie[2]), 2)
+    movies_Rated.append([title, release_date, average_rating])
+
+movies_Rated.sort(key=lambda x: x[2], reverse=True)
+
+for movie in movies_Rated:
+    print(f"{movie[0]} ({movie[1]}) - Avergae rating: {movie[2]}⭐")
