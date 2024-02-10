33d48276c36115ff3cc46874281f36055eb3a659
Maha <mahaalqarni.offficial@gmail.com>
2024-02-10 03:33:21 +0300

    Last commit

--- tuples.py
+++ tuples.py
@@ -1,13 +1,11 @@
 
 
 num_list = [ 2, 3, 4, 5, 15, 1, 43, 20 ]
-print(num_list)
-
 def element_sum(elements) :
     return sum(elements)
 
 total_sum = element_sum(num_list)
-print("Sum:", total_sum)
+print("Sum of list elements:", total_sum)
 
 def numbers(elements):
     return max(elements)
@@ -16,7 +14,7 @@ largest_number = numbers(num_list)
 print("The largest number:", largest_number)
 
 odd_numers = [num for num in range(1200 , 2000, 125)if num % 2 != 0]
-print(odd_numers)
+print("Odd numbers list:" , odd_numers)
 
-new_list = odd_numers[:2]
-print(new_list)
+new_list = odd_numers[:5]
+print("Slicing:" , new_list)
