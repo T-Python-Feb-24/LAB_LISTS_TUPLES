148206f515c3582bfbd9017c3df4a69d74763e0b
Fai <faiabdulla123@gmail.com>
2024-02-10 19:12:16 +0300

    list,tuple

---
+++ bonus.py
@@ -0,0 +1,5 @@
+Moive_list(title,releaseyear,rating)
+avrage_list=sum (Moive_list)/len(Moive_list)
+highest_list=max(Moive_list)
+lowest_list=min(Moive_list)
+# not finished yet
---
+++ list,tuple.py
@@ -0,0 +1,9 @@
+def sum_of_list(input_list):
+    return sum(input_list)
+def find_largest_number(input_list):
+    return max(list)
+odd_numbers_list = [num for num in range(1200, 2001, 125) if num % 2 != 0]
+new_list = odd_numbers_list[:5]  
+
+
+ 
